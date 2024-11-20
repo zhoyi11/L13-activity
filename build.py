48bc17b9333ae3ee3a7e889a912a0c9c8c6ca1c7
Noah Smith <nsmith2@unc.edu>
2024-11-20 07:06:30 +0000

    Add basic files for demo

---
+++ build.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+import os
+
+def update_time():
+    # Read in the file
+    with open("./index.html", "r") as file:
+        filedata = file.read()
+
+    # Get the current time and place it in the html file
+    current_time = datetime.now()
+    filedata = filedata.replace("CURRENT_TIME_HERE", str(current_time))
+
+    # Make the "out" folder
+    os.makedirs("./out", exist_ok=True)
+
+    # Write the file to the "out" folder
+    with open("./out/index.html", "w") as file:
+        file.write(filedata)
+
+
+update_time()
